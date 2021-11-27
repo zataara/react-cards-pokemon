e93f1f2b835a4225271b08075f9968220c81b9e5
zataara <bryantmac@gmail.com>
2021-11-27 11:05:25 -0800

    built custom hook for flipping cards

---
+++ src/hooks/useFlip.js
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+
+
+const useFlip = () => {
+    const [state, setState] = useState(true);
+    const flipState = () => {
+        setState(state => !state)
+    }
+    return [state, flipState]
+}
+
+export default useFlip;
