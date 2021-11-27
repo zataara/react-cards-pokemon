0a532fae22958420f90e42468249b82fda6e3e22
zataara <bryantmac@gmail.com>
2021-11-27 11:05:37 -0800

    added custom hook for flipping card

--- src/PokemonCard.js
+++ src/PokemonCard.js
@@ -1,12 +1,10 @@
 import React, { useState } from "react";
+import useFlip from './hooks/useFlip'
 import "./PokemonCard.css";
 
 /* Renders a single pokemon card. */
 function PokemonCard({ front, back, name, stats }) {
-  const [isFacingUp, setIsFacingUp] = useState(true);
-  const flipCard = () => {
-    setIsFacingUp(isUp => !isUp);
-  };
+  const [isFacingUp, flipCard] = useFlip();
   return (
     <div onClick={flipCard} className="PokemonCard Card">
       {isFacingUp ? (
