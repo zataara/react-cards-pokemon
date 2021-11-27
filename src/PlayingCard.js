e8ac965b4f25267f6748f3c4bf47b7783bb1fc90
zataara <bryantmac@gmail.com>
2021-11-27 11:06:38 -0800

    added custom hook for flipping card

--- src/PlayingCard.js
+++ src/PlayingCard.js
@@ -1,10 +1,11 @@
 import React, { useState } from "react";
+import useFlip from './hooks/useFlip'
 import backOfCard from "./back.png";
 import "./PlayingCard.css"
 
 /* Renders a single playing card. */
 function PlayingCard({ front, back = backOfCard }) {
-  const [isFacingUp, setIsFacingUp] = useState(true);
+  const [isFacingUp, setIsFacingUp] = useFlip();
   const flipCard = () => {
     setIsFacingUp(isUp => !isUp);
   };
