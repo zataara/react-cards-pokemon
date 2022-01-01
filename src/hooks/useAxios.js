d11683064045202d302327328eef9505914c0115
zataara <bryantmac@gmail.com>
2022-01-01 10:41:47 -0800

    added more custom hooks

--- src/hooks/useAxios.js
+++ src/hooks/useAxios.js
@@ -1,13 +1,14 @@
-import React, { useState } from 'react';
-
+import { useState } from "react";
+import axios from "axios";
 
 const useAxios = (url) => {
-    const[responses, setResponses] = useState();
-
-    async function addResponseData(url) {
-        const res = await axios.get(url)
-        setResponses(data => [...data, ])
-    }
-}
+  const [responses, setResponses] = useState();
+  async function addResponseData(url) {
+    const res = await axios.get(url);
+    setResponses((responses) => [...responses, res.data]);
+  }
+  const clearResponses = () => setResponses([])
+  return [responses, addResponseData, clearResponses];
+};
 
 export default useAxios;
